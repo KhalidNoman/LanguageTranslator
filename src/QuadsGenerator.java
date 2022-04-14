0bb693a9b9bf4cfbfc8e3c0b164f26628c60c393
Khalid <ksnoman@live.com>
2022-04-14 01:50:34 -0500

    Syntax needs tuning and then producing code

--- src/QuadsGenerator.java
+++ src/QuadsGenerator.java
@@ -40,25 +40,62 @@ public class QuadsGenerator {
 
         int lastOp = 0;
         for(int i = 0; i < tokens.size(); i++){
-            System.out.println(tokens.get(i) + " " + i);
-            if(!headers.contains(tokens.get(i).item)){
+            if(!headers.contains(tokens.get(i).item) && !tokens.get(i).item.equals(",")){
                 myStack.add(tokens.get(i));
             } else {
-                System.out.println("ELSE");
                 if(lastOp != 0){
+                    //System.out.println("CHECKING: " + myStack.get(lastOp).item  + " WITH " + tokens.get(i).item );
+                    if((myStack.get(lastOp).item.equals(";") && tokens.get(i).item.equals(",")) || (myStack.get(lastOp).item.equals(",") && tokens.get(i).item.equals(";"))
+                    || (myStack.get(lastOp).item.equals(";") && tokens.get(i).item.equals(";")) || (myStack.get(lastOp).item.equals(",") && tokens.get(i).item.equals(","))){
+                        i++;
+                        myStack.remove(lastOp);
+                        for(int x = myStack.size() -1; x > -1; x--){
+                            if(headers.contains(myStack.get(x).item)){
+                                lastOp = x;
+                                break;
+                            }
+                        }
+                    } else
+                    if(pTable[headers.indexOf(myStack.get(lastOp).item)][tokens.get(i).item.equals(",")? 0:headers.indexOf(tokens.get(i).item)] == 2){
+                        System.out.println("REDUCE " + myStack.get(lastOp).item + " takes " + tokens.get(i).item);
 
-                    if(pTable[headers.indexOf(myStack.get(lastOp).item)][headers.indexOf(tokens.get(i).item)] == 2){
-                        System.out.println("REDUCE " + myStack.get(lastOp).item + " > " + tokens.get(i).item);
+                        if(myStack.get(lastOp).type.equals("&mulop") || myStack.get(lastOp).type.equals("&addop")){
+                            for(int x = 0; x < 4; x++){
+                                System.out.println("REMOVE " + myStack.remove(myStack.size()-1));
+                            }
+                        } else if(myStack.get(lastOp).type.equals("&relop") || myStack.get(lastOp).type.equals("$assign")){
+                            for(int x = 0; x < 3; x++){
+                                System.out.println("REMOVE " + myStack.remove(myStack.size()-1));
+                            }
+                        } else if(myStack.get(lastOp).item.equals("$CALL")){
+                            for(int x = 0; x < 2; x++){
+                                System.out.println("REMOVE " + myStack.remove(myStack.size()-1));
+                            }
+                        } else if(myStack.get(lastOp).item.equals(")")){
+                            for(int x = 0; x < 2; x++){
+                                System.out.println("REMOVE " + myStack.remove(myStack.size()-1));
+                            }
+                        }
+
+                        for(int x = myStack.size() -1; x > -1; x--){
+                            if(headers.contains(myStack.get(x).item)){
+                                lastOp = x;
+                                System.out.println("NEW LAST: "  + myStack.get(lastOp));
+                                break;
+                            }
+                        }
+                        myStack.add(tokens.get(i));
+                        if(headers.contains(tokens.get(i).item))
+                            lastOp = myStack.size()-1;
                     } else {
                         myStack.add(tokens.get(i));
-                        lastOp = myStack.size();
+                        lastOp = myStack.size() - 1;
                     }
                 }else {
                     myStack.add(tokens.get(i));
-                    lastOp = myStack.size();
+                    lastOp = myStack.size() - 1;
                 }
             }
-            System.out.println("STACK " + myStack.toString());
         }
 
         System.out.println(myStack.toString());
